29f496f1a7a1d2cbf63be7cef8b62107ec97443a
maxiee <maxieewong@gmail.com>
2023-01-31 13:15:31 +0800

    init script

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+alias info-listview="$DIR/python/info-listview/info-listview.py"
+alias rss-info="$DIR/python/rss-info/rss-info.py"
+
