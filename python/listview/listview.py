11cd4dd2168a1344ac074fe3d97ce532f9a6c0c4
maxiee <maxieewong@gmail.com>
2023-01-31 10:12:28 +0800

    listview

--- python/listview/listview.py
+++ python/listview/listview.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListView, QListWidget, QListWidgetItem
+import json
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListView, QListWidget, QListWidgetItem, QWidget, QVBoxLayout, QLabel
+
+data = []
+
+if len(sys.argv) > 1:
+    data = json.loads(sys.argv[1])
+elif sys.stdin:
+    data = json.loads(str(sys.stdin.read()))
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setWindowTitle('listview')
 
-listview = QListView()
 listwidget = QListWidget()
 
-for i in range(10):
-    item = QListWidgetItem(f'Item {i}')
+
+class InfoCard(QWidget):
+    def __init__(self, data, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout(self)
+        title = QLabel(data['title'])
+        description = QLabel(data['description'])
+        layout.addWidget(title)
+        layout.addWidget(description)
+        self.setLayout(layout)
+
+
+for i in range(len(data)):
+    item = QListWidgetItem(listwidget)
+    card = InfoCard(data[i], parent=listwidget)
+    item.setSizeHint(card.sizeHint())
     listwidget.addItem(item)
+    listwidget.setItemWidget(item, card)
 
 
 def on_item_clicked(index):
     print("Item clicked", index.row())
 
 
-listview.clicked.connect(lambda index: on_item_clicked(index))
+listwidget.clicked.connect(lambda index: on_item_clicked(index))
 
-listview.setModel(listwidget.model())
-
-window.setCentralWidget(listview)
+window.setCentralWidget(listwidget)
 window.show()
 sys.exit(app.exec_())
